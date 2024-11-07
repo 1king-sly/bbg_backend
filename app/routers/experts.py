d0ada67d846d8724837c7f962c82155602ddd7ee
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-11-07 10:36:28 +0300

    debugging vercel errors

--- app/routers/experts.py
+++ app/routers/experts.py
@@ -6,15 +6,31 @@ from app.core.security import get_password_hash
 from app.schemas.expert import ExpertCreate, ExpertUpdate, Expert
 from app.models.expert import Expert as ExpertModel
 
+from db import prisma
+
 router = APIRouter(prefix="/experts", tags=["experts"])
 
 @router.post("/", response_model=Expert)
-def create_expert(expert: ExpertCreate, db: Session = Depends(get_db)):
+async def create_expert(expert: ExpertCreate, db: Session = Depends(get_db)):
+    # existing_expert = await prisma.expert.find_unique(
+    #     where={"email": expert.email},
+    # )
+    #
+    # if existing_expert:
+    #     raise HTTPException(status_code=400, detail="Email already registered")
     db_expert = db.query(ExpertModel).filter(ExpertModel.email == expert.email).first()
     if db_expert:
         raise HTTPException(status_code=400, detail="Email already registered")
     
     hashed_password = get_password_hash(expert.password)
+
+    # new_expert = await prisma.expert.create(
+    #     email=expert.email,
+    #     name=expert.name,
+    #     phone=expert.phone,
+    #     fieldOfExpertise=expert.fieldOfExpertise,
+    #     password = hashed_password
+    # )
     db_expert = ExpertModel(
         **expert.dict(exclude={"password"}),
         hashed_password=hashed_password
