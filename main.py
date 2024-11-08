feacf02fa0e0b363163f2013ce55e38eb0a13952
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-11-08 16:11:57 +0300

    fixing render deploy errors

--- main.py
+++ main.py
@@ -46,7 +46,3 @@ async def root():
     return {"message": "Hello From  Baby Gal"}
 
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
